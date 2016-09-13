0cece478c1df3f5f7472e7175851cd546ea9a5bb
Shotaro Sasaki <fspia.biz@gmail.com>
2016-09-14 00:21:07 +0900

    fin.

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
 import os
+import sys
 from bottle import route, run, template, request
+from summpy.lexrank import summarize
 
 @route('/')
 def index():
     frm = """
-        <h1>Hello</h1>
+        <h1>Paste Review!</h1>
         <form action="/sum" method="post">
-            <input type="text" name="review" />
-            <input type="submit" name="sb" value="DO" />
+            <p><textarea name="review" rows="25", cols="80"></textarea></p>
+            <p><input type="submit" value="   summarize   " /></p>
         </form>
     """
     return template(frm)
 
 @route('/sum', method="POST")
 def sum():
-    review = request.forms.get("review")
-    return template('<b>{{review}}</b>', review=review)
+    review = unicode(request.forms.get("review"), 'utf-8')
+    try:
+   	    sentences, debug_info = summarize(review, sent_limit=3, continuous=True, debug=True)
+    except:
+        print sys.exc_info()[0]
+        raise
+
+    sumres = ""
+    for sent in sentences:
+        sumres = sumres + sent
+
+    out = """
+        <h1>Summarize Result.</h1>
+        <p>[REVIEW]</p>
+        <p><textarea name="review" rows="25", cols="80">{{review}}</textarea></p>
+        <p></p>
+        <p>[RESULT]</p>
+        <p><textarea name="sumres" rows="15", cols="80">{{sumres}}</textarea></p>
+    """
+    return template(out, review=review, sumres=sumres)
 
 run(host='0.0.0.0', port=int(os.environ.get("PORT", 5364)))
 
